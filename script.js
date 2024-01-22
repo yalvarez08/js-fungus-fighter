8436cfd550c5b5af0e519136189b8764af746f0c
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-01-22 18:44:29 -0500

    added onclickin html and added js comments

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 
 function onReady() {
     console.log("Ready to go!")
-    
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
@@ -16,5 +15,38 @@ function onReady() {
     // - Rendered to the DOM
 }
 
-
 onReady()
+
+//TO-DOs HandleClick events:
+//1. update state variables to make the Fungus lose hit points, 
+//and to reduce your attack points (AP)
+//2.  State may be held in one our more variables 
+//3. HP and AP values may not be negative 
+//  (set to zero, if they would otherwise be negative)
+function handleArcaneClick() {
+
+
+}
+
+function handleEntangleClick() {
+
+
+}
+
+function handleDragonClick() {
+
+}
+
+function handleStarClick() {
+
+
+}
+
+//TO-DO: Render state changes to DOM
+// Update the text above the attack buttons (eg, "100 AP")
+// Update the text above the enemy fungus 
+// set condition--If the Fungus runs out of HP, its dead & you win
+// Replace the walk class with a dead class on the freaky-fungus element to make it die. 
+
+// set condition #2--If you run out of AP, the monster wins 
+// Replace the walk class with jump class on the freaky-fungus element.
